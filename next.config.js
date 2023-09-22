c7bb7e218ffd8648db4e23207300825ff061fa0c
PacificD <1484636442@qq.com>
2023-09-22 14:26:43 +0800

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 const { withContentlayer } = require('next-contentlayer')
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+}
 
 module.exports = withContentlayer(nextConfig)
